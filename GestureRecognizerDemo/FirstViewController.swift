3f457aebd30b0c66b8a6c3af603dea36e98c4601
Patrick M. Roskam <pat.roskam@gmail.com>
2018-04-13 15:18:36 -0700

    initial commit

---
+++ GestureRecognizerDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GestureRecognizerDemo
+//
+//  Created by Patrick Roskam on 4/13/18.
+//  Copyright © 2018 PatrickMRoskam. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GestureRecognizerDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GestureRecognizerDemo
+//
+//  Created by Patrick Roskam on 4/13/18.
+//  Copyright © 2018 PatrickMRoskam. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
