5ea4a0a53e8061f5a4251e90c550e41f1f5e1d9f
Patrick M. Roskam <pat.roskam@gmail.com>
2018-04-14 08:52:36 -0700

    tested before adding actions

--- GestureRecognizerDemo/FirstViewController.swift
+++ GestureRecognizerDemo/FirstViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    var squareView: UIView!
 
     override func viewDidLoad() {
+        
+        squareView = UIView(frame: CGRect(x: 150, y: 250, width: 100, height: 100))
+        squareView.backgroundColor = .blue
+        view.addSubview(squareView)
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(recognizer:)))
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
+        let rotateGesture = UIRotationGestureRecognizer(target: self, action: #selector(handleRotation(recognizer:)))
+        let pinchGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePinch(recognizer:)))
+        
+        squareView.gestureRecognizers = [panGesture, tapGesture, rotateGesture, pinchGesture]
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +36,30 @@ class FirstViewController: UIViewController {
     }
 
 
+    @objc func handlePan(recognizer: UIPanGestureRecognizer) {
+        let translationInView = recognizer.translation(in: self.view)
+        recognizer.view!.center = CGPoint(x:view.center.x + translationInView.x, y:view.center.y + translationInView.y)
+    }
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        recognizer.view?.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
+    
+    @objc func handleRotation(recognizer: UIRotationGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = view.transform.rotated(by: recognizer.rotation)
+            recognizer.rotation = 0
+        }
+    }
+    
+    @objc func handlePinch(recognizer : UIPinchGestureRecognizer) {
+        if let view = recognizer.view {
+            view.transform = view.transform.scaledBy(x: recognizer.scale, y: recognizer.scale)
+            recognizer.scale = 1
+        }
+    }
 }
 
